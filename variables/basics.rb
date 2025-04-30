ec85bde4925c4f43b47fc430b5568d786c5d3ee7
Md Abu Zafar <mdabuzafar78@gmail.com>
2025-04-30 16:45:30 +0530

    Add basics.rb for introductory Ruby examples
    Created a new file containing basic Ruby examples demonstrating the use of print and puts, as well as variable declaration and string interpolation. This serves as a foundational resource for beginners in the Ruby programming crash course.

---
+++ variables/basics.rb
@@ -0,0 +1,18 @@
+# print and puts
+puts "Hello, World!"
+print "Hello, World!"
+print "Hello,"
+print "World!"
+
+puts
+puts "--------------------------------"
+
+
+# variables
+character_name = "John"
+character_age = "35"
+
+puts "There once was a man named " + character_name
+puts "He was " + character_age + " years old"
+puts "He really liked the name " + character_name
+puts "But didn't like being " + character_age
