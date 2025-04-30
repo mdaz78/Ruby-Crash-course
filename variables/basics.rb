66e77e2651be93ee14a1de3ca3380b16bb12ed74
Md Abu Zafar <mdabuzafar78@gmail.com>
2025-04-30 16:50:28 +0530

    Add data type examples to basics.rb
    Expanded the basics.rb file by including examples of various data types in Ruby, such as strings, integers, floats, booleans, and nil. This enhancement provides additional foundational knowledge for beginners in the Ruby programming crash course.

--- variables/basics.rb
+++ variables/basics.rb
@@ -16,3 +16,18 @@ puts "There once was a man named " + character_name
 puts "He was " + character_age + " years old"
 puts "He really liked the name " + character_name
 puts "But didn't like being " + character_age
+
+
+# data types
+# string
+name = "John"
+# integer
+age = 35
+# float
+gpa = 3.5
+# boolean
+is_student = true
+# nil
+is_nil = nil
+
+puts "--------------------------------"
